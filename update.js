bc939b55b3706007ac7a05b3d057f43a9b54724b
=siraom15 <=siriwatjaiyungyuen@gmail.com>
2020-05-16 10:43:38 +0700

    fix

--- update.js
+++ update.js
@@ -1,14 +1,20 @@
-
 var fs = require('fs')
 var token = fs.readFileSync('token.txt', 'utf8');
-console.log('Get token success');
+if(token!=""){
+    console.log('Get token success');
 
-var update = fs.readFileSync('template.js', 'utf8');
-update = update.replace('token', token);
-fs.writeFile('temp.js', update, (err) => {
-    if (err) console.log(err);
-    console.log('Updated token');
-});
+    var update = fs.readFileSync('template.js', 'utf8');
+    update = update.replace('token', token);
+    fs.writeFile('temp.js', update, (err) => {
+        if (err) console.log(err);
+        console.log('Updated token');
+    });
+}
+else{
+    console.log("Set your bot token before");
+    
+}
+
 
 
 
